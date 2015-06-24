2a79c7dc0ffa4ca4e9106a4742db6f1988f1d108
Tu Rash <tu.rash@ymail.com>
2015-06-24 13:47:07 -0700

    assignment23completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,6 @@
 require_relative "entry.rb"
+require "csv"
+
 
 class AddressBook
     attr_accessor :entries
@@ -21,7 +23,17 @@ class AddressBook
         @entries.insert(index, Entry.new(name, phone, email))
         
     end
-    
+
+    def import_from_csv(file_name)
+       csv_text = File.read(file_name)
+       csv = CSV.parse(csv_text, headers: true)
+       
+       csv.each do |row|
+           row_hash = row.to_hash
+           add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+        end
+    end
+
     def get_entry(number)
         
         if number < @entries.length
